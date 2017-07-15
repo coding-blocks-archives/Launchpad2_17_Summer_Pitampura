32ca631121bd962495dfe66ba46cbeed405729af
Deepak Aggarwal <daggarwal77@gmail.com>
2017-07-15 19:53:32 +0530

    Queues and Generic Trees with N children

--- 08-Jul_13/decodingOOPS.cpp
+++ 08-Jul_13/decodingOOPS.cpp
@@ -4,21 +4,21 @@
 #include <iostream>
 using namespace std;
 
-class Square{
+class Square {
 
 public:
 	int dim;
-	Square(){
+	Square() {
 		cout << "Inside Empty Ctor Square()\n";
 		dim = 0;
 	}
 
-	Square(const Square& X){
+	Square(const Square& X) {
 		cout << "Inside Copy Ctor Square(Square& X)\n";
 		dim = X.dim;
 	}
 
-	Square add(Square X){
+	Square add(Square X) {
 		cout << "Inside add" << endl;
 		Square tmp(*this);
 		// cout << "I win\n";
@@ -26,7 +26,7 @@ public:
 		return tmp;
 	}
 
-	Square& operator=(const Square& S){
+	Square& operator=(const Square& S) {
 		Square tmp;
 		cout << "Inside operator= \n";
 		dim = S.dim;
@@ -38,7 +38,7 @@ public:
 
 
 
-int main(){
+int main() {
 	Square S1;
 	Square S2;
 
@@ -49,7 +49,8 @@ int main(){
 	// cout << "Class confused :(\n";
 	// S3.dim = 5;
 
-	S3 = S2 = S1;
+	S3 = S2 = S1;	//S3.operator = (S2.operator = (S1));
+	Square S3 = S2 = S1;		//S3(S2.operator=(S1));---initialisation hence copy ctor
 
 
 
